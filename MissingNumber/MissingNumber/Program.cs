90d15b302bf4ccbffb79ac517c48ff8a05b11793
Aishwarya2909 <79073260+Aishwarya2909@users.noreply.github.com>
2021-05-10 17:51:25 +0530

    Add files via upload

---
+++ MissingNumber/MissingNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MissingNumber
+{
+    class Program
+    {
+        static int getMissingNo(int[] A, int n)
+        {
+            int total = (n + 1) * (n + 2) / 2;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+                sum = sum + A[i];
+            total = total - sum;
+            return total;
+        }
+
+        public static void Main()
+
+        {
+            int[] A = { 1, 2, 4, 5, 6 };
+            int miss = getMissingNo(A, 5);
+            Console.WriteLine(miss);
+        }
+    }
+}   
